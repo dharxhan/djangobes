7f048f9107318daba163b31d3abd28832028264b
dharxhan <dharshan.ig20@bitsathy.ac.in>
2024-12-03 09:33:05 +0530

    [feat: Added a loop function to the table ]

--- project/sample/myapp/urls.py
+++ project/sample/myapp/urls.py
@@ -5,4 +5,5 @@ from . import views
 urlpatterns = [
     path("app/", views.index, name="index"),
     path("ctable/", views.ctable, name="ctable"),
+     path("table2/", views.table2, name="table2"),
 ]
--- project/sample/myapp/views.py
+++ project/sample/myapp/views.py
@@ -228,3 +228,84 @@ button {
 </body>
 </html>'''
     return HttpResponse(mywebsite)
+
+
+def table2(request):
+    sNum = [1, 2, 3, 4, 5]
+    eNames = ['Mohan', "Raju", "Krishna", "Krish", "Kruthika"]
+    eSalary = [60000, 70000, 50000, 40000, 800000]
+    eDisg = ['SE', 'SSE', 'SE', 'SE', 'SSE']
+    eOrg = ['Intel', 'Dell', 'Siemens', 'Accenture', 'Bosch']
+
+    mywebsite = '''<!DOCTYPE html>
+<html>
+<head>
+<meta name="viewport" content="width=device-width, initial-scale=1">
+<title> Login Page </title>
+<style>
+body {
+  font-family: Calibri, Helvetica, sans-serif;
+  background-color: pink;
+}
+button {
+       background-color: #4CAF50;
+       width: 100%;
+        color: orange;
+        padding: 15px;
+        margin: 10px 0px;
+        border: none;
+        cursor: pointer;
+         }
+ form {
+        border: 3px solid #f1f1f1;
+    }
+ input[type=text], input[type=password] {
+        width: 100%;
+        margin: 8px 0;
+        padding: 12px 20px;
+        display: inline-block;
+        border: 2px solid green;
+        box-sizing: border-box;
+    }
+ button:hover {
+        opacity: 0.7;
+    }
+  .cancelbtn {
+        width: auto;
+        padding: 10px 18px;
+        margin: 10px 5px;
+    }
+
+ .container {
+        padding: 25px;
+        background-color: lightblue;
+    }
+</style>
+</head>
+<body>
+
+     <table border="1px" cellspacing="1px" cellpadding="5px" rules="all">
+        <tr>
+            <th>Snum</th>
+            <th>eNames</th>
+            <th>eSalary</th>
+            <th>eDisg</th>
+            <th>eOrg</th>
+        </tr>
+        <tbody>'''
+    for sno, name, desig,  org, sal in zip(sNum, eNames, eDisg, eOrg, eSalary):
+        mywebsite=mywebsite+"<tr>"
+        mywebsite=mywebsite+'<td>'+str(sno)+'</td>'
+        mywebsite=mywebsite+'<td>'+name+'</td>'
+        mywebsite=mywebsite+'<td>'+desig+'</td>'
+        mywebsite=mywebsite+'<td>'+org+'</td>'
+        mywebsite=mywebsite+'<td>'+str(sal)+'</td>'
+
+
+    mywebsite = mywebsite + '''</tbody>
+
+    </tbody>
+    </table>
+</body>
+</html>'''
+    return HttpResponse(mywebsite)
