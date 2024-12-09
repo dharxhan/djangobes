55d2c7e63af48f5c032994066f4c476586a066de
dharxhan <dharshan.ig20@bitsathy.ac.in>
2024-12-09 22:56:14 +0530

    [fix: Mariadb reinstalled connected to the server along with the table]

--- database/connectdb/dbapp/models.py
+++ database/connectdb/dbapp/models.py
@@ -1,3 +1,9 @@
 from django.db import models
 
 # Create your models here.
+class student(models.Model):
+    rno=models.IntegerField()
+    name=models.CharField(max_length=30)
+    marks=models.FloatField()
+    city=models.CharField(max_length=30)
+    fees=models.FloatField()
--- database/connectdb/dbapp/views.py
+++ database/connectdb/dbapp/views.py
@@ -1,3 +1,11 @@
 from django.shortcuts import render
+from .models import*
 
 # Create your views here.
+def adddata(requests):
+    sObj= Student()
+    sObj.rno= 100
+    sObj.name="Mohan"
+    sObj.marks=450
+    sObj.fees=100000
+    sObj.city="Bangalore"
