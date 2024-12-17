a4ad6ff34ea6df109649c64b3709aac30a318f00
dharxhan <dharshan.ig20@bitsathy.ac.in>
2024-12-17 11:16:08 +0530

    [feat: added employee function to th RestAPI model]

--- RestAPI/RestAPIapp/urls.py
+++ RestAPI/RestAPIapp/urls.py
@@ -2,5 +2,6 @@ from django.urls import path
 from . import views
 
 urlpatterns = [
-    path('say_hello/', views.say_hello, name='say_hello')
+    path('say_hello/', views.say_hello, name='say_hello'),
+    path('getapi/', views.getdata, name='getdata'),
 ]
--- RestAPI/RestAPIapp/views.py
+++ RestAPI/RestAPIapp/views.py
@@ -1,8 +1,16 @@
 from django.shortcuts import render
 from rest_framework.response import Response
 from rest_framework.decorators import api_view
+from .models import Employee
+from .serializers import EmployeeSerializer
 # Create your views here.
 
-@api_view()
+#@api_view(['GET'])
 def say_hello(request):
     return Response({"statuscode":200, "message": "Hello World!"})
+
+@api_view(['GET'])
+def getdata(request):
+    employees = Employee.objects.all()
+    serializer = EmployeeSerializer(employees, many=True)
+    return Response(serializer.data)
